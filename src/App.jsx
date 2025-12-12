5d388e118f64bc2a67c98350e5259399aa578107
hawkiee <gokulaudhayannv@gmail.com>
2025-12-12 12:07:43 +0530

    changes in page

--- src/App.jsx
+++ src/App.jsx
@@ -10,35 +10,34 @@ import Header from './components/Header.jsx';
 
 const App = () => (
   <>
-  <Header />
-  <ReactFullpage
-    // This is where you configure the vertical scroll and navigation
-    scrollingSpeed={700}
-    navigation={true}
-    navigationTooltips={['Home', 'About', 'Projects', 'Contact']}
-    showActiveTooltip={true}
-    controlArrows={true} // Add this to enable horizontal arrows for slides within a section
-    render={({ state, fullpageApi }) => {
-      return (
-        
-        <ReactFullpage.Wrapper>
-          
-          <div className="section">
-            <Home />
-          </div>
-          <div className="section">
-            <About />
-          </div>
-          <div className="section">
-            <ProjectSection />
-          </div>
-          <div className="section">
-            <ContactSection />
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
+    <ReactFullpage
+      // This is where you configure the vertical scroll and navigation
+      scrollingSpeed={700}
+      navigation={true}
+      navigationTooltips={['Home', 'About', 'Projects', 'Contact']}
+      showActiveTooltip={true}
+      controlArrows={true} // Add this to enable horizontal arrows for slides within a section
+      render={({ state, fullpageApi }) => {
+        return (
+
+          <ReactFullpage.Wrapper>
+
+            <div className="section">
+              <Home />
+            </div>
+            <div className="section">
+              <About />
+            </div>
+            <div className="section">
+              <ProjectSection />
+            </div>
+            <div className="section">
+              <ContactSection />
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
   </>
 );
 
